26ae627f809cc6420a36cb12e8fa201d8bcfa917
ManXClague <90554981+ManXClague@users.noreply.github.com>
2020-12-16 08:20:03 +0000

    Add changeset

--- app/src/main/java/com/nowiczenkoandrzej/e_bazarek/data/models/ProductResponse.kt
+++ app/src/main/java/com/nowiczenkoandrzej/e_bazarek/data/models/ProductResponse.kt
@@ -2,3 +2,6 @@ package com.nowiczenkoandrzej.e_bazarek.data.models
 
 data class ProductResponse(
     val description: String = "",
+    val id: String = "",
+    val images: List<String> = emptyList(),
+    val owner: S
