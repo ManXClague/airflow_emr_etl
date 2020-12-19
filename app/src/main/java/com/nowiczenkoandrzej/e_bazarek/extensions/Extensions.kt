25657487b188b7b599b3b9ab4f49db670135f6df
ManXClague <90554981+ManXClague@users.noreply.github.com>
2020-12-19 16:19:11 +0000

    Bump rake from 12.3.2 to 13.0.1

--- app/src/main/java/com/nowiczenkoandrzej/e_bazarek/extensions/Extensions.kt
+++ app/src/main/java/com/nowiczenkoandrzej/e_bazarek/extensions/Extensions.kt
@@ -5,4 +5,6 @@ import android.content.Intent
 import android.view.View
 import android.view.animation.AlphaAnimation
 import android.widget.Toast
-import androidx.appcompat.app.AppCompat
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
+import androidx.fragment.app.Fragment
