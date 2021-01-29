c085bf59c6c070d9f1b8925c10549581ae8ab18c
ManXClague <90554981+ManXClague@users.noreply.github.com>
2021-01-29 06:08:32 +0000

    Version 0.9.8

--- app/src/main/java/com/nowiczenkoandrzej/e_bazarek/presentation/activities/ContentActivity.kt
+++ app/src/main/java/com/nowiczenkoandrzej/e_bazarek/presentation/activities/ContentActivity.kt
@@ -3,4 +3,6 @@ package com.nowiczenkoandrzej.e_bazarek.presentation.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.findNavController
-import androidx.navigation.ui.Ap
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+i
