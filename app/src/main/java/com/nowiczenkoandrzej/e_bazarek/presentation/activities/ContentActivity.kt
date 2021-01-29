f6d8d3a27e7bbce3534be3de45d6149b9f1eee65
ManXClague <90554981+ManXClague@users.noreply.github.com>
2021-01-29 05:55:39 +0000

    Uploaded presentation

--- app/src/main/java/com/nowiczenkoandrzej/e_bazarek/presentation/activities/ContentActivity.kt
+++ app/src/main/java/com/nowiczenkoandrzej/e_bazarek/presentation/activities/ContentActivity.kt
@@ -1,4 +1,6 @@
 package com.nowiczenkoandrzej.e_bazarek.presentation.activities
 
 import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundl
+import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.Ap
