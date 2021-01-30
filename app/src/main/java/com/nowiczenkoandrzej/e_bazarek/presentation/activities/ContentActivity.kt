4ea6c6414f81976d751f1868caa7f09bea3bb2b7
ManXClague <90554981+ManXClague@users.noreply.github.com>
2021-01-30 21:55:02 +0000

    tools: ffs-test: fix header values endianess

--- app/src/main/java/com/nowiczenkoandrzej/e_bazarek/presentation/activities/ContentActivity.kt
+++ app/src/main/java/com/nowiczenkoandrzej/e_bazarek/presentation/activities/ContentActivity.kt
@@ -26,4 +26,6 @@ class ContentActivity : AppCompatActivity() {
             R.id.favoritesFragment
         ))
 
-        val 
+        val navController = findNavController(R.id.nav_host)
+
+        setupActionBarWithNavController(n
