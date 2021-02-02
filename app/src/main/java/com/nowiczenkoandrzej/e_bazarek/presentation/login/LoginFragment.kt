b635e92cc04708cb5facc6a05cb6592e22e50b5c
ManXClague <90554981+ManXClague@users.noreply.github.com>
2021-02-02 21:25:47 +0000

    post2

--- app/src/main/java/com/nowiczenkoandrzej/e_bazarek/presentation/login/LoginFragment.kt
+++ app/src/main/java/com/nowiczenkoandrzej/e_bazarek/presentation/login/LoginFragment.kt
@@ -3,4 +3,7 @@ package com.nowiczenkoandrzej.e_bazarek.presentation.login
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
-import androi
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+im
