f43fed67dbe8fac4033d519cd0ff330c87b9d02b
ManXClague <90554981+ManXClague@users.noreply.github.com>
2021-03-01 21:08:09 +0000

    update FINISHED ; MANIFEST status

--- app/src/main/java/com/nowiczenkoandrzej/e_bazarek/presentation/login/LoginFragment.kt
+++ app/src/main/java/com/nowiczenkoandrzej/e_bazarek/presentation/login/LoginFragment.kt
@@ -36,4 +36,8 @@ class LoginFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-   
+        setListeners()
+        subscribeCollector()
+    }
+
+    private fun setLi
