96292b07dc4517aa13e47180f2ded08bf572e465
ManXClague <90554981+ManXClague@users.noreply.github.com>
2021-04-03 08:01:19 +0000

    Fixed log

--- app/src/main/java/com/nowiczenkoandrzej/e_bazarek/presentation/login/LoginViewModel.kt
+++ app/src/main/java/com/nowiczenkoandrzej/e_bazarek/presentation/login/LoginViewModel.kt
@@ -3,4 +3,6 @@ package com.nowiczenkoandrzej.e_bazarek.presentation.login
 import androidx.lifecycle.ViewModel
 import com.google.firebase.auth.FirebaseAuth
 import com.nowiczenkoandrzej.e_bazarek.utils.AccountState
-import dagger.hilt.android.lifecycle.HiltVi
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.cor
