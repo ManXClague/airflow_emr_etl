33edc4ad0102ac7600ccb99973c6682f8ea09642
ManXClague <90554981+ManXClague@users.noreply.github.com>
2021-04-18 06:19:41 +0000

    Version 1.0.2

--- app/src/main/java/com/nowiczenkoandrzej/e_bazarek/presentation/products/ProductsAdapter.kt
+++ app/src/main/java/com/nowiczenkoandrzej/e_bazarek/presentation/products/ProductsAdapter.kt
@@ -27,4 +27,6 @@ class ProductsAdapter(): RecyclerView.Adapter<ProductsAdapter.ViewHolder>() {
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.image.setImageResource(R.drawable.ic_baseline_default_avatar)
         holder.title.text = products[position].title
-        holder.description.text = products[position].des
+        holder.description.text = products[position].description
+        holder.price.text = "$${products[position].price.toString()}"
+ 
