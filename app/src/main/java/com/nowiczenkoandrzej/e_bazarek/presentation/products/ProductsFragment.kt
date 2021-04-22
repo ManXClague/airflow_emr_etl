e2a48c05425b77da6c61644e02fa15fd02aa6c2c
ManXClague <90554981+ManXClague@users.noreply.github.com>
2021-04-22 19:48:20 +0000

    Add more fields

--- app/src/main/java/com/nowiczenkoandrzej/e_bazarek/presentation/products/ProductsFragment.kt
+++ app/src/main/java/com/nowiczenkoandrzej/e_bazarek/presentation/products/ProductsFragment.kt
@@ -2,4 +2,7 @@ package com.nowiczenkoandrzej.e_bazarek.presentation.products
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import 
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.vie
