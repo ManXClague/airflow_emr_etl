42799c21ae91185bcc2d72d2b1c0918406067eac
ManXClague <90554981+ManXClague@users.noreply.github.com>
2021-06-03 11:19:32 +0000

    Fix seeds

--- app/src/main/java/com/nowiczenkoandrzej/e_bazarek/presentation/profile/ProfileFragment.kt
+++ app/src/main/java/com/nowiczenkoandrzej/e_bazarek/presentation/profile/ProfileFragment.kt
@@ -4,4 +4,5 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.*
 import android.view.animation.AnimationUtils
-import
+import androidx.appcompat.app.AlertDialog
+import androidx.core.view.MenuHo
