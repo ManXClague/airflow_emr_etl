b3396b145e95c1a32b4148af7fc0e767699b589a
ManXClague <90554981+ManXClague@users.noreply.github.com>
2021-07-04 07:22:48 +0000

    [fixed] stability issue

--- app/src/main/java/com/nowiczenkoandrzej/e_bazarek/presentation/profile/ProfileFragment.kt
+++ app/src/main/java/com/nowiczenkoandrzej/e_bazarek/presentation/profile/ProfileFragment.kt
@@ -39,4 +39,5 @@ class ProfileFragment : Fragment() {
         return binding.root
     }
 
-    override fun onViewCreated(view: View, savedInst
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
