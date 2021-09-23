a180517b2497350ab14b6fc7a407047019e25836
ManXClague <90554981+ManXClague@users.noreply.github.com>
2021-09-23 13:17:00 +0000

    format code according to conventions [skip ci]

--- app/src/main/java/com/nowiczenkoandrzej/e_bazarek/presentation/profile/ProfileViewModel.kt
+++ app/src/main/java/com/nowiczenkoandrzej/e_bazarek/presentation/profile/ProfileViewModel.kt
@@ -3,4 +3,5 @@ package com.nowiczenkoandrzej.e_bazarek.presentation.profile
 import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.ViewModel
 import com.nowiczenkoandrzej.e_bazarek.domain.FirebaseRepository
-impor
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.asSta
