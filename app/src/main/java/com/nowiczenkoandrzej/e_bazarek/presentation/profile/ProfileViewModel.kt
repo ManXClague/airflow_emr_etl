9eab45b953e61d5e0a4dfc9ef0ea8d27ec4c2116
ManXClague <90554981+ManXClague@users.noreply.github.com>
2021-09-25 06:30:01 +0000

    Changed path

--- app/src/main/java/com/nowiczenkoandrzej/e_bazarek/presentation/profile/ProfileViewModel.kt
+++ app/src/main/java/com/nowiczenkoandrzej/e_bazarek/presentation/profile/ProfileViewModel.kt
@@ -4,4 +4,9 @@ import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.ViewModel
 import com.nowiczenkoandrzej.e_bazarek.domain.FirebaseRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.asSta
+import kotlinx.coroutines.flow.asStateFlow
+import javax.inject.Inject
+
+@HiltViewModel
+class ProfileViewModel
+@Inject 
