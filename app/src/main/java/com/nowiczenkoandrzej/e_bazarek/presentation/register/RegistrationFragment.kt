71fad4a4fcd1cad29fcc3701387b91d4f8099419
ManXClague <90554981+ManXClague@users.noreply.github.com>
2021-09-26 21:39:45 +0000

    rtlwifi: rtl8192cu: Fix some code in RF handling

--- app/src/main/java/com/nowiczenkoandrzej/e_bazarek/presentation/register/RegistrationFragment.kt
+++ app/src/main/java/com/nowiczenkoandrzej/e_bazarek/presentation/register/RegistrationFragment.kt
@@ -4,4 +4,6 @@ import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
-impor
+import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Li
