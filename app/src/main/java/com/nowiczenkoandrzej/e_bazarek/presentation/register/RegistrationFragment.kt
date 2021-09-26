2cb1e9b459da56285a0e2a0912ada1196a7b00c5
ManXClague <90554981+ManXClague@users.noreply.github.com>
2021-09-26 18:44:17 +0000

    / ‘projects/DragonHeart_KR/index.md’

--- app/src/main/java/com/nowiczenkoandrzej/e_bazarek/presentation/register/RegistrationFragment.kt
+++ app/src/main/java/com/nowiczenkoandrzej/e_bazarek/presentation/register/RegistrationFragment.kt
@@ -1,4 +1,7 @@
 package com.nowiczenkoandrzej.e_bazarek.presentation.register
 
 import android.os.Bundle
-import 
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+impor
