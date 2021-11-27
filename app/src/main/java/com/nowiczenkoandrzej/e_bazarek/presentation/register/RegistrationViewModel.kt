d6c91a1a30dd4ea9be0d1f725861d3b9dc36cfef
ManXClague <90554981+ManXClague@users.noreply.github.com>
2021-11-27 06:14:22 +0000

    Nits

--- app/src/main/java/com/nowiczenkoandrzej/e_bazarek/presentation/register/RegistrationViewModel.kt
+++ app/src/main/java/com/nowiczenkoandrzej/e_bazarek/presentation/register/RegistrationViewModel.kt
@@ -3,4 +3,6 @@ package com.nowiczenkoandrzej.e_bazarek.presentation.register
 import androidx.lifecycle.ViewModel
 import com.google.firebase.auth.FirebaseAuth
 import com.nowiczenkoandrzej.e_bazarek.utils.AccountState
-import dagger.hilt.android.lifecycle
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.fl
