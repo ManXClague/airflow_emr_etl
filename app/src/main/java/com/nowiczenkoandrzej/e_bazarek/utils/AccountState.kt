6b023078cfd76fb1612db1903e787a5d36c05156
ManXClague <90554981+ManXClague@users.noreply.github.com>
2021-12-13 07:31:45 +0000

    Add diagram

--- app/src/main/java/com/nowiczenkoandrzej/e_bazarek/utils/AccountState.kt
+++ app/src/main/java/com/nowiczenkoandrzej/e_bazarek/utils/AccountState.kt
@@ -4,4 +4,5 @@ sealed class AccountState{
     object Success : AccountState()
     class Failure(val error: String): AccountState()
     object Loading: AccountState()
-  
+    object Empty: AccountState()
+}
