197796874d83323abfe476fca54eb3c255e77a4e
ManXClague <90554981+ManXClague@users.noreply.github.com>
2023-02-07 19:46:28 +0000

    Bump version to 1.8

--- gradlew.bat
+++ gradlew.bat
@@ -30,4 +30,6 @@ set APP_BASE_NAME=%~n0
 set APP_HOME=%DIRNAME%
 
 @rem Resolve any "." and ".." in APP_HOME to make it shorter.
-for %%i in ("%APP_HOME%") do set APP_H
+for %%i in ("%APP_HOME%") do set APP_HOME=%%~fi
+
+@rem Add default JVM options here. You can also use JAVA_OPTS and GRADLE_OPTS to 
